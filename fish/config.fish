491c0bd69b2de6420e3cd91780dac37a2a62055b
Nik Gupta <vngupta77@gmail.com>
2023-04-03 14:35:06 +0100

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Autojump
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
